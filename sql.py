6c92f556dbd5b1e1c06e9c8ee5661e9fa52c863d
EmirhanBarlas <emirhanbarlas9@gmail.com>
2024-01-19 23:26:52 +0300

    add age and city table.

--- sql.py
+++ sql.py
@@ -4,8 +4,8 @@ conn = sqlite3.connect('veritabani.db')
 c = conn.cursor()
 c.execute('''
     CREATE TABLE kisiler
-    (isim TEXT, soyisim TEXT)
+    (isim TEXT, soyisim TEXT, yas INTEGER, sehir TEXT)
 ''')
-c.execute("INSERT INTO kisiler VALUES ('Ahmet', 'Mehmet')")
+c.execute("INSERT INTO kisiler VALUES ('Emirhan', 'Pelin', 30, 'Istanbul')")
 conn.commit()
 conn.close()
