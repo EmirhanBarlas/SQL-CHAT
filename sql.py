197cf94f0128544486c84e1ef03f393a8c0c17d1
EmirhanBarlas <emirhanbarlas9@gmail.com>
2024-01-19 23:27:39 +0300

    add commit message

--- sql.py
+++ sql.py
@@ -1,11 +1,11 @@
-import sqlite3
+import sqlite3 # Sqlite3 kütüphanesini dahil ediyoruz.
 
-conn = sqlite3.connect('veritabani.db')
-c = conn.cursor()
+conn = sqlite3.connect('veritabani.db') # veritabani.db adında bir veritabanı dosyası oluşturuyoruz.
+c = conn.cursor() # Veritabanı üzerinde işlem yapabilmek için bir imleç oluşturuyoruz.
 c.execute('''
     CREATE TABLE kisiler
     (isim TEXT, soyisim TEXT, yas INTEGER, sehir TEXT)
-''')
-c.execute("INSERT INTO kisiler VALUES ('Emirhan', 'Pelin', 30, 'Istanbul')")
-conn.commit()
-conn.close()
+''') # Veritabanında kisiler tablosunu oluşturuyoruz.
+c.execute("INSERT INTO kisiler VALUES ('Emirhan', 'Pelin', 30, 'Istanbul')") # Veritabanına veri ekliyoruz.
+conn.commit() # Veritabanı üzerindeki değişiklikleri kaydediyoruz.
+conn.close() # Veritabanı bağlantısını kapatıyoruz.
